8833931c31aea7b15b9ea2864a04fa91cae96a88
Pecz <balint.pecz@sap.com>
2022-09-18 22:07:26 +0200

    Done.

--- 11-regex/exercises/03/solution/js/validateMACAddress.js
+++ 11-regex/exercises/03/solution/js/validateMACAddress.js
@@ -1,1 +1,6 @@
+const validateMACAddress = (MACAddress) => {
+    const pattern = /^(([A-F0-9]{2})-|:){5}([A-F0-9]{2})$/i;
+    return pattern.test(MACAddress);
+};
+
 export default validateMACAddress;
